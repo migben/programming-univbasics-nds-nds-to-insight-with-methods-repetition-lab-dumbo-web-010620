881997d869a80224e396db80a2d18a8fbd5b58a0
Miguel Ben <itsmigben@gmail.com>
2019-12-11 09:07:03 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,34 +1,60 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
+# Enjoyed the Nth-Order Method example on the Intro. It's a popular reference that's used quite often in the course of Harvard CS50. 
+# Unable to use the multi line comment format =begin =end 
 
-def directors_totals(source)
+def directors_totals(src)
+  
   result = {}
   director_index = 0
-  while director_index < source.size do
-    director = source[director_index]
+  
+  while director_index < src.size
+    
+    director = src[director_index]
     result[director[:name]] = gross_for_director(director)
     director_index += 1
+    
   end
+  
   result
 end
 
 def gross_for_director(d)
+  
   total = 0
   index = 0
 
-  while index < d[:movies].length do
+  while index < d[:movies].size
+  
     total += d[:movies][index][:worldwide_gross]
     index += 1
+    
   end
 
   total
+  
 end
 
-def list_of_directors(source)
+#######################################################
+#                   My code is below                  #
+#######################################################
+
+def list_of_directors(src)
   # Write this implementation
+  director_list = []
+  idx = 0
+  
+  while idx < src.size
+    
+    director_list << src[idx][:name]
+    idx += 1
+    
+  end
+  
+  director_list
 end
 
-def total_gross(source)
+def total_gross(src)
   # Write this implementation
   #
   # Should use methods:
@@ -38,6 +64,22 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  
+  grand_total, idx = 0, 0 # the same as assigning grand_total and idx on their own line.
+  
+  dir_totals = directors_totals(src) # => hash={dir1: total_gross, dir2: total_gross,...., dirNth: total_gross}  
+  
+  director_names = list_of_directors(src) # [director1, director2,.....,directorN]
+  
+  while idx < src.size
+  
+    grand_total +=  dir_totals[director_names[idx]] # => hash[dir_name] = total_gross
+    idx  +=1
+    
+  end
+  
+  grand_total
+  
 end
 
 
